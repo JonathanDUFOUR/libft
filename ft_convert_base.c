e6bc813a1a1742d3dea5c48c857f74e4913fd582
jodufour <jodufour@student.42.fr>
2021-03-07 22:37:10 +0100

    fixed up convert_base

--- ft_convert_base.c
+++ ft_convert_base.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_convert_base.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdufour <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 06:21:58 by jdufour           #+#    #+#             */
-/*   Updated: 2021/03/06 01:48:31 by jonathan         ###   ########.fr       */
+/*   Updated: 2021/03/07 22:35:38 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 char	*ft_convert_base(char *n, char *bf, char *bt)
 {
+	if (!n)
+		return (NULL);
 	return (ft_itoa_base(ft_atoi_base(n, bf), bt));
 }
--- ft_wrong_base.c
+++ ft_wrong_base.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_wrong_base.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdufour <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 06:28:43 by jdufour           #+#    #+#             */
-/*   Updated: 2020/04/02 14:29:13 by jdufour          ###   ########.fr       */
+/*   Updated: 2021/03/07 19:48:59 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_bool	ft_wrong_base(char const *base)
 	size_t	i;
 	size_t	j;
 
-	if (ft_strlen(base) < 2)
+	if (!base || ft_strlen(base) < 2)
 		return (TRUE);
 	i = 0;
 	while (base[i])
