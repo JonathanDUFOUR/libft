56f819f311083d027aca347911b6b5e1d370d1cc
jodufour <jodufour@student.42.fr>
2021-04-07 03:19:12 +0200

    Added second version of ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,17 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 06:27:46 by jdufour           #+#    #+#             */
-/*   Updated: 2021/03/23 17:25:50 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/04/07 03:18:31 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <sys/types.h>
+#include "libft.h"
 
+/*
+**	1 malloc version
+*/
+/*
 static char	**populate(char **output, char *ptr, char const *s, char c)
 {
 	size_t	size;
@@ -51,4 +55,68 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	output[size] = NULL;
 	return (populate(output, (char *)(output + size + 1), s, c));
+}
+*/
+
+/*
+**	N malloc(s) version
+*/
+
+static unsigned int	words_count(char const *s, char c)
+{
+	unsigned int	count;
+	int				new_word;
+
+	count = 0;
+	new_word = 1;
+	while (*s && *s == c)
+		++s;
+	while (*s)
+	{
+		if (*s != c && new_word)
+		{
+			new_word = 0;
+			++count;
+		}
+		if (*s == c)
+			new_word = 1;
+		++s;
+	}
+	return (count);
+}
+
+static size_t	word_len(char const *s, char c)
+{
+	register char const	*p = s;
+
+	while (*p && *p != c)
+		++p;
+	return (p - s);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char			**output;
+	char			**p;
+	unsigned int	count;
+	size_t			w_len;
+
+	if (!s)
+		return (NULL);
+	count = words_count(s, c);
+	output = malloc((count + 1) * sizeof(char *));
+	if (!output)
+		return (NULL);
+	p = output;
+	while (count--)
+	{
+		while (*s == c)
+			++s;
+		w_len = word_len(s, c);
+		*p = ft_calloc((w_len + 1), sizeof(char));
+		ft_strncpy(*p++, s, w_len);
+		s += w_len;
+	}
+	*p = NULL;
+	return (output);
 }
