841b596562f9a383ad6fd315f3859c092fcb66bf
jodufour <jodufour@student.42.fr>
2021-04-07 18:38:16 +0200

    norminette OK

--- ft_putbytes.c
+++ ft_putbytes.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 18:12:50 by jodufour          #+#    #+#             */
-/*   Updated: 2021/04/07 18:35:09 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/04/07 18:37:48 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 void	ft_putbytes(void *addr, size_t size)
 {
-	
 	while (size--)
 	{
 		ft_putbyte(*((uint8_t *)(addr + size)));
